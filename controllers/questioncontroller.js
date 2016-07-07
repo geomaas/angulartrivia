e4cd740b2194c5919115aeeeb3dc84377abdef82
geomaas <geoff.maas@gmail.com>
2016-07-06 23:39:08 -0400

    questions groups added

--- controllers/questioncontroller.js
+++ controllers/questioncontroller.js
@@ -1,5 +1,13 @@
 module.exports = function(jeopardy) {
     jeopardy.controller('GameController', ['$scope','QuestionService', 'LoginService', function($scope, QuestionService, LoginService) {
+      $scope.players = LoginService.getPlayerArr();
+      $scope.catOne = QuestionService.getOneArr();
+      $scope.catTwo = QuestionService.getTwoArr();
+      $scope.catThree = QuestionService.getThreeArr();
+      $scope.catFour = QuestionService.getFourArr();
+      $scope.catFive = QuestionService.getFiveArr();
 
+      console.log("test", $scope.players);
+      console.log($scope.catThree);
     }]);
   };
--- public/app.js
+++ public/app.js
@@ -35,7 +35,15 @@ module.exports = function(jeopardy) {
 },{}],3:[function(require,module,exports){
 module.exports = function(jeopardy) {
     jeopardy.controller('GameController', ['$scope','QuestionService', 'LoginService', function($scope, QuestionService, LoginService) {
+      $scope.players = LoginService.getPlayerArr();
+      $scope.catOne = QuestionService.getOneArr();
+      $scope.catTwo = QuestionService.getTwoArr();
+      $scope.catThree = QuestionService.getThreeArr();
+      $scope.catFour = QuestionService.getFourArr();
+      $scope.catFive = QuestionService.getFiveArr();
 
+      console.log("test", $scope.players);
+      console.log($scope.catThree);
     }]);
   };
 
@@ -106,31 +114,85 @@ module.exports = function(jeopardy) {
 
 module.exports = function(jeopardy) {
     jeopardy.factory('QuestionService', function($http) {
-      let newQs = [];
+      let categoryOneArr = [];
+      let categoryTwoArr = [];
+      let categoryThreeArr = [];
+      let categoryFourArr = [];
+      let categoryFiveArr = [];
+
       let oldQs = [];
         $http({
             method: 'GET',
-            url: 'http://jservice.io/api/categories?count=10',
+            url: 'http://jservice.io/api/clues',
         }).then(function(response) {
-          console.log(response);
-            let question = response.data[0];
-            console.log(question);
-            // // console.log("NewQuestion click working");
-            // $scope.questiongroup.question = question.question;
-            // $scope.questiongroup.value = question.value;
-            // $scope.questiongroup.category = question.category.title;
-            // // $scope.questiongroup.answer = question.answer;
-            // $scope.previousquestion.push({
-            //     question: question.question,
-            //     value: question.value,
-            //     answer: question.answer,
-            //     userscore: 0
-            // });
-            // $scope.playerAnswer.val('');
-            return question;
+          // console.log(response);
+            let category = response.data;
+            // console.log(category);
+            categoryOne = category.filter(function (el) {
+              return el.category_id === 1;
+            });
+
+            categoryTwo = category.filter(function (el) {
+              return el.category_id === 7;
+            });
+
+            categoryThree = category.filter(function (el) {
+              return el.category_id === 3;
+            });
+
+            categoryFour = category.filter(function (el) {
+              return el.category_id === 4;
+            });
+
+            categoryFive = category.filter(function (el) {
+              return el.category_id === 11;
+            });
+            // console.log(categoryOne);
+            angular.copy(categoryOne, categoryOneArr);
+            angular.copy(categoryTwo, categoryTwoArr);
+            angular.copy(categoryThree, categoryThreeArr);
+            angular.copy(categoryFour, categoryFourArr);
+            angular.copy(categoryFive, categoryFiveArr);
+
+            console.log(categoryOneArr,categoryFiveArr,categoryTwoArr,categoryThreeArr,categoryFourArr);
+
+            return 0;
         });
-        return newQs;
+        return {
+          getOneArr: function() {
+            return categoryOneArr;
+          },
+          getTwoArr: function() {
+            return categoryTwoArr;
+          },
+          getThreeArr: function() {
+            return categoryThreeArr;
+          },
+          getFourArr: function() {
+            return categoryFourArr;
+          },
+          getFiveArr: function() {
+            return categoryFiveArr;
+          },
+        }
     });
 };
+
+
+
+
+
+// // console.log("NewQuestion click working");
+// $scope.questiongroup.question = question.question;
+// $scope.questiongroup.value = question.value;
+// $scope.questiongroup.category = question.category.title;
+// // $scope.questiongroup.answer = question.answer;
+// $scope.previousquestion.push({
+//     question: question.question,
+//     value: question.value,
+//     answer: question.answer,
+//     userscore: 0
+// });
+// $scope.playerAnswer.val('');
 
 },{}]},{},[4])
